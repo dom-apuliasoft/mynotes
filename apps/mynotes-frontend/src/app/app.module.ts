914943e7e2bdc6201b2d94b63986698b1ad32ae0
dom-apuliasoft <domenico.mancini@apuliasoft.com>
2021-11-18 13:58:25 +0100

    Cleanup frontend

--- apps/mynotes-frontend/src/app/app.module.ts
+++ apps/mynotes-frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NoteComponent } from './note/note.component';
 import { NotespaceComponent } from './notespace/notespace.component';
 import { NotespaceViewComponent } from './notespace-view/notespace-view.component';
 import { NotesModalComponent } from './notes-modal/notes-modal.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { NotesModalComponent } from './notes-modal/notes-modal.component';
     NotespaceComponent,
     NotespaceViewComponent,
     NotesModalComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
