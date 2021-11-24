4d4e0ba646f99658305ed1d2a3a274e1b73431a5
dom-apuliasoft <domenico.mancini@apuliasoft.com>
2021-11-24 12:03:56 +0100

    Fix validation messages

--- apps/mynotes-frontend/src/app/form-error/form-error.component.ts
+++ apps/mynotes-frontend/src/app/form-error/form-error.component.ts
@@ -11,10 +11,35 @@ export class FormErrorComponent {
   @Input() control: AbstractControl | null = null;
 
   get errorMessage(): string {
-    if (this.control?.touched && this.control.errors)
-      return JSON.stringify(this.control.errors);
+    const errors = this.control?.errors;
 
-    return '';
+    if (!this.control?.touched || !errors) return '';
+
+    const messages: string[] = [];
+
+    Object.entries(errors).forEach(entry => {
+      const [prop, value] = entry;
+
+      switch (prop) {
+        case 'required':
+          messages.push(`Field is required`);
+          break;
+        case 'minlength':
+          console.log(value);
+          messages.push(`Must be more than ${value.requiredLength} characters`);
+          break;
+        case 'maxlength':
+          messages.push(`Must be less than ${value.requiredLength} characters`);
+          break;
+        default: {
+          const msg = value.message ?? value;
+          messages.push(msg.toString());
+          break;
+        }
+      }
+    });
+
+    return messages.join(', ');
   }
 
 }
